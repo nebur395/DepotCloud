446eb0c53a175271fd82afc631658884d12de2e8
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-04-08 11:21:38 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    swaggerJSDoc = require("swagger-jsdoc"),
+    crypto = require("crypto"),
+    fs = require("fs"),
+    https = require("https");
+
+
+var app = express();
+
+app.use(express.static('./public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.models = require('./models');
+
+require('./routes')(app);
+
+app.use('/', function(req, res) {
+    console.log("Bienvenido");
+});
+
+// Database connection and server launching
+
+var dbUri = 'mongodb://localhost:27017/aniscloDb';
+mongoose.connect(dbUri);
+mongoose.connection.once('open', function(){
+
+    console.log("MongoDB connection created in "+dbUri);
+
+    app.listen(8080, function(){
+        console.log("Server listening to PORT 8080");
+    });
+
+});
+
+module.exports = app;
