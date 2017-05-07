03865e2e92ca41031f1bd7991a342b4c6990e079
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-05-07 22:40:40 +0200

    Added notification angular service

--- public/javascript/services/notificationService.js
+++ public/javascript/services/notificationService.js
@@ -1,3 +1,48 @@
-/**
- * Created by ruben on 7/05/17.
- */
+angular.module('depotCloudApp')
+
+    // 'notification' service manage the ui-notifications of the client side
+    .factory('notificationService', function (Notification) {
+
+        return {
+            // show the error message
+            showError: function (title, message, delay) {
+                if (!delay) {
+                    Notification.error({title: title, message: message});
+                } else if (!title) {
+                    Notification.error({message: message, delay: delay});
+                } else if (!title && !delay) {
+                    Notification.error(message);
+                } else {
+                    Notification.error({title: title, message: message, delay: delay});
+                }
+            },
+
+            // show the success message
+            showSuccess: function (title, message, delay) {
+                if (!delay) {
+                    Notification.success({title: title, message: message});
+                } else if (!title) {
+                    Notification.success({message: message, delay: delay});
+                } else if (!title && !delay) {
+                    Notification.success(message);
+                } else {
+                    Notification.success({title: title, message: message, delay: delay});
+                }
+            },
+
+            // show the warning message
+            showWarning: function (title, message, delay) {
+                if (!delay) {
+                    Notification.warning({title: title, message: message});
+                } else if (!title) {
+                    Notification.warning({message: message, delay: delay});
+                } else if (!title && !delay) {
+                    Notification.warning(message);
+                } else {
+                    Notification.warning({title: title, message: message, delay: delay});
+                }
+            }
+        };
+    });
+
+
