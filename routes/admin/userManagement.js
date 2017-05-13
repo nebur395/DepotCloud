9c0b9a6ce1cf2ca6878d5cea91ca288fbee21145
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-05-13 19:05:57 +0200

    Added activate user with admin privileges in the back-end

--- routes/admin/userManagement.js
+++ routes/admin/userManagement.js
@@ -116,5 +116,95 @@ module.exports = function (app) {
         });
     });
 
+    /**
+     * @swagger
+     * /admin/users/{email}/active:
+     *   put:
+     *     tags:
+     *       - Admin
+     *     summary: Reactiva una cuenta de usuario.
+     *     description: Permite a un administrador emplear sus todopoderosas
+     *      bolas de dragón para reactivar una cuenta de usuario que éste
+     *      había borrado.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: email
+     *         description: Email del usuario que sirve como identificador.
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       403:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por acceder
+     *           a operaciones de administrador sin los privilegios necesarios.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       404:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
+    router.put("/users/:email/active", function (req, res) {
+
+        if (!req.user.admin) {
+            res.status(403).send({
+                "success": false,
+                "message": "No estás autorizado a acceder a esta operación."
+            });
+            return;
+        }
+
+        User.findOneAndUpdate({email: req.params.email}, {
+            isActive: true,
+            deactivationDate: null
+        }, function (err, result) {
+
+            if (err) {
+                res.status(500).send({
+                    "success": false,
+                    "message": "Error interno del servidor."
+                });
+                return;
+            }
+
+            if (result) {
+                res.status(200).send({
+                    "success": true,
+                    "message": "Cuenta de usuario reactivada correctamente."
+                });
+            } else {
+                res.status(404).send({
+                    "success": false,
+                    "message": "El usuario no existe."
+                });
+            }
+        });
+
+    });
+
     return router;
 };
