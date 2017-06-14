481f325ab124f3e7ea4c7db2f1cc594b3ad124a9
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-14 13:19:49 +0200

    Added 'delete depot' http request definition

--- routes/depot/depot.js
+++ routes/depot/depot.js
@@ -142,7 +142,7 @@ module.exports = function (app) {
      *     tags:
      *       - Depot
      *     summary: Modificar un almacén.
-     *     description: Modifica un almacén de una unidad familiar..
+     *     description: Modifica un almacén de una unidad familiar.
      *     consumes:
      *       - application/json
      *       - charset=utf-8
@@ -163,7 +163,7 @@ module.exports = function (app) {
      *         type: string
      *       - name: name
      *         description: Nombre del almacén.
-     *         in: body
+     *         in: path
      *         required: true
      *         type: string
      *       - name: location
@@ -208,6 +208,60 @@ module.exports = function (app) {
      *           $ref: '#/definitions/FeedbackMessage'
      */
     router.put("/:owner/:name", function (req, res) {
+
+    });
+
+    /**
+     * @swagger
+     * /depots/{owner}/{name}:
+     *   delete:
+     *     tags:
+     *       - Depot
+     *     summary: Eliminar un almacén.
+     *     description: Elimina un almacén de una unidad familiar.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: email
+     *         description: Email de la unidad familiar a la que pertenecen los almacenes.
+     *         in: path
+     *         required: true
+     *         type: string
+     *       - name: name
+     *         description: Nombre del almacén.
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       404:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
+    router.delete("/:owner/:name", function (req, res) {
 
     });
 
