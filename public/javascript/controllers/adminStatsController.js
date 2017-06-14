c8df6d78d7153b05652d29981e5e464acdeffcd0
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-14 12:12:36 +0200

    Added test of websockets 2

--- public/javascript/controllers/adminStatsController.js
+++ public/javascript/controllers/adminStatsController.js
@@ -1,5 +1,15 @@
 angular.module('depotCloudApp')
 
     .controller('adminStatsCtrl', ['$scope', function ($scope) {
+        function updateStats(memuse) {
+            document.getElementById('rss').innerHTML = memuse.rss;
+            document.getElementById('heapTotal').innerHTML = memuse.heapTotal;
+            document.getElementById('heapUsed').innerHTML = memuse.heapUsed;
+        }
 
+        var host = window.document.location.host.replace(/:.*/, '');
+        var ws = new WebSocket('ws://' + host + ':8080/websocketStats');
+        ws.onmessage = function (event) {
+            updateStats(JSON.parse(event.data));
+        };
     }]);
