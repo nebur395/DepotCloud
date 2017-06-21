032e4470f0b2ef271b7b868a1abf4fd2134a60b4
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-21 11:47:42 +0200

    Added totalDepotObjects server side stat

--- routes/admin/stats.js
+++ routes/admin/stats.js
@@ -125,5 +125,62 @@ module.exports = function (app) {
         });
     });
 
+    /**
+     * @swagger
+     * /adminStats/totalDepotObjects:
+     *   get:
+     *     tags:
+     *       - AdminStats
+     *     summary: Número de objetos totales del sistema
+     *     description: Devuelve el número de objetos totales registrados en el sistema.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *     responses:
+     *       200:
+     *         description: Número de objetos totales del sistema.
+     *         schema:
+     *           type: object
+     *           properties:
+     *              totalDepotObjects:
+     *               type: integer
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
+    router.get("/totalDepotObjects", function(req, res){
+
+        if (!req.user.admin) {
+            return res.status(401).send({
+                "success": false,
+                "message": "No estás autorizado a acceder."
+            });
+        }
+
+        Depot.count(function(err, depotObjects){
+            if(err) {
+                return res.status(500).send({
+                    "success": false,
+                    "message": "Error recuperando datos"
+                });
+            }
+
+            return res.status(200).send({
+                "totalDepotObjects": depotObjects
+            });
+        });
+    });
+
     return router;
 };
