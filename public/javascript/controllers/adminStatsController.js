41b27d1a0bff39e6867f11a729589b10fdb03409
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-23 00:49:06 +0200

    Added totalUser, totalDepots, and totalDepotObjects client side stats

--- public/javascript/controllers/adminStatsController.js
+++ public/javascript/controllers/adminStatsController.js
@@ -1,7 +1,49 @@
 angular.module('depotCloudApp')
 
-    .controller('adminStatsCtrl', ['$scope', function ($scope) {
+    .controller('adminStatsCtrl', ['$scope', 'notificationService', 'statsService',
+        function ($scope, notificationService, statsService) {
+
+            $scope.optionsPieStat = {
+                legend: {display: true},
+                responsive: true,
+                maintainAspectRatio: false
+            };
+
+            $scope.optionsRadarStat = {
+                responsive: true, maintainAspectRatio: false,
+                scale: {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            };
+
+            $scope.optionsLineStat = {
+                scales: {
+                    yAxes: [{
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        position: 'left',
+                        ticks: {min: 0, max:100}
+                    }]
+                }
+            };
 
 
+            // SECTION: Total User Stat, Total Depots Stat, Total DepotObjects Stat
+            $scope.labels1Stat = ["Usuarios", "Almacenes", "Objetos"];
+            $scope.data1Stat = [0, 0, 0];
 
-    }]);
+            statsService.getTotalUsers(function (users) {
+                $scope.data1Stat[0] = users;
+            }, notificationService.showError);
+
+            statsService.getTotalDepots(function (depots) {
+                $scope.data1Stat[1] = depots;
+            }, notificationService.showError);
+
+            statsService.getTotalDepotObjects(function (depotObjects) {
+                $scope.data1Stat[2] = depotObjects;
+            }, notificationService.showError);
+
+        }]);
---
+++ public/javascript/services/statsService.js
@@ -0,0 +1,53 @@
+angular.module('depotCloudApp')
+
+// 'statsService' service manage the stats functions of the page with the server
+    .factory('statsService', function ($http, authService) {
+
+        return {
+
+            // GET request that get the totalUsers stats
+            getTotalUsers: function (callbackSuccess, callbackError) {
+                $http({
+                    method: 'GET',
+                    url: 'adminStats/totalUsers',
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    }
+                }).then(function (successData) {
+                    callbackSuccess(successData.data.totalUsers);
+                }, function (errorData) {
+                    callbackError('&#10008',errorData.data.message);
+                });
+            },
+
+            // GET request that get the totalDepots stats
+            getTotalDepots: function (callbackSuccess, callbackError) {
+                $http({
+                    method: 'GET',
+                    url: 'adminStats/totalDepots',
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    }
+                }).then(function (successData) {
+                    callbackSuccess(successData.data.totalDepots);
+                }, function (errorData) {
+                    callbackError('&#10008',errorData.data.message);
+                });
+            },
+
+            // GET request that get the totalDepotObjects stats
+            getTotalDepotObjects: function (callbackSuccess, callbackError) {
+                $http({
+                    method: 'GET',
+                    url: 'adminStats/totalDepotObjects',
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    }
+                }).then(function (successData) {
+                    callbackSuccess(successData.data.totalDepotObjects);
+                }, function (errorData) {
+                    callbackError('&#10008',errorData.data.message);
+                });
+            }
+        };
+    });
