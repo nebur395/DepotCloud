4655157878a0bb66c4fe619abc84a0e5fb9d70e1
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-22 14:32:56 +0200

    Added depotsPerUser tests

--- test/mocha/statsTests/depotsPerUser.js
+++ test/mocha/statsTests/depotsPerUser.js
@@ -1,3 +1,89 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var server = require('../../../server');
+var createUserToken = require('../jwtCreator').createUserToken;
+var createUser = require('../userCreator').createUser;
+var deleteUser = require('../userCreator').deleteUser;
+var createDepot = require('../depotCreator').createDepot;
+var deleteDepots = require('../depotCreator').deleteDepots;
+
+chai.use(chaiHttp);
+
 /**
- * Created by ruben on 22/06/17.
+ * Test suite for Stats functionalities.
  */
+describe('Stats', function () {
+
+    var name = "testUser";
+    var email = "testUser@email.com";
+    var password = "testPass";
+    var depotsId = [];
+
+    /*
+     * It creates a new user before the test suite starts executing.
+     */
+    before(function (done) {
+
+        createUser(name, false, email, password, ["Pepe"], function () {
+
+            createDepot("Depot name", email, "Depot Location", "Storage Room", "[0-1km]",
+                "Depot Description", depotsId, done);
+        });
+
+    });
+
+    /**
+     * Tests for depotsPerUser stat.
+     */
+    describe("#depotsPerUser()", function () {
+
+        it('should successfully get the depotsPerUser stats', function (done) {
+
+            chai.request(server)
+                .get('/adminStats/depotsPerUser')
+                .set('Authorization','Bearer ' + createUserToken(name, true))
+                .end(function (err, result) {
+
+                    result.should.have.status(200);
+                    result.body.should.be.a('object');
+                    result.body.should.have.property('depotsPerUser');
+
+                    done();
+
+                });
+        });
+
+        it('should return an error since user isn\'t an admin', function (done) {
+
+            chai.request(server)
+                .get('/adminStats/depotsPerUser')
+                .set('Authorization','Bearer ' + createUserToken(name, false))
+                .end(function (err, result) {
+
+                    result.should.have.status(403);
+                    result.body.should.be.a('object');
+                    result.body.should.have.property('success');
+                    result.body.success.should.equal(false);
+                    result.body.should.have.property('message');
+                    result.body.message.should.equal('No estás autorizado a acceder a esta operación.');
+
+                    done();
+
+                });
+        });
+    });
+
+
+    /*
+     * Removes the user created at the begining of the tests
+     * after every test is finished.
+     */
+    after(function (done) {
+
+        deleteDepots(depotsId, function () {
+            deleteUser(email, done);
+        });
+
+    });
+});
