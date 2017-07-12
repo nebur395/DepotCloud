41f70f83945d9de6e9ec3469ec945c172363d07a
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-07-12 21:13:01 +0200

    Ionic client: Added members page with add and get methods

---
+++ public-ionic/src/pages/members/members-page.component.ts
@@ -0,0 +1,95 @@
+import {Component}                            from '@angular/core';
+import {ModalController, ToastController}     from 'ionic-angular';
+
+import { MemberCreatePageComponent } from '../members-create/members-create-page.component';
+
+import {MemberService}  from '../../providers/member.service';
+
+import {Observable} from "rxjs/Observable";
+
+@Component({
+  selector: 'members-page',
+  templateUrl: 'members-page.component.html'
+})
+export class MembersPageComponent {
+  currentMembers: string[];
+
+  constructor(
+    private modalCtrl: ModalController,
+    private memberService: MemberService,
+    private toastCtrl: ToastController
+  ) { }
+
+  /**
+   * The view loaded, let's query our items for the list
+   */
+  ionViewDidLoad() {
+    this.memberService.getMembers().then(
+      (members: string[]) => {
+        this.currentMembers = members
+      }
+    );
+  }
+
+  /**
+   * Prompt the user to add a new mmber. This shows our MemberCreatePageComponent in a
+   * modal and then adds the new member to our data source if the user created one.
+   */
+  addMember(): void {
+    let addModal = this.modalCtrl.create(MemberCreatePageComponent);
+
+    addModal.onDidDismiss((member: string) => {
+      if (member) {
+        this.memberService.add(member).then(
+          (observable: Observable<any>) => {
+            observable.subscribe(
+              (resp) => {
+
+                let jsonResp = resp.json();
+
+                // User created
+                let toast = this.toastCtrl.create({
+                  message: jsonResp.message,
+                  position: 'bottom',
+                  duration: 4000,
+                  cssClass: 'toast-success'
+                });
+                toast.present();
+                this.currentMembers.push(member);
+
+
+              }, (err) => {
+
+                let jsonErr = err.json();
+
+                // Unable to sign up
+                let toast = this.toastCtrl.create({
+                  message: jsonErr.message,
+                  position: 'bottom',
+                  duration: 4000,
+                  cssClass: 'toast-error'
+                });
+                toast.present();
+
+              });
+          }
+        );
+      }
+    });
+    addModal.present();
+  }
+
+  /**
+   * Delete an item from the list of items.
+   */
+  deleteMember(member: string): void {
+
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  selectMember(member: string): void {
+
+  }
+}
