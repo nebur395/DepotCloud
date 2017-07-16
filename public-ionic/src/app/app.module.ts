a468f3ce3cd00fc37c1d591e9d6182372545c9b0
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-07-16 14:21:44 +0200

    Ionic client: Added depotObjectsCreate page mock

--- public-ionic/src/app/app.module.ts
+++ public-ionic/src/app/app.module.ts
@@ -6,28 +6,29 @@ import { IonicStorageModule }                       from '@ionic/storage';
 
 import { AppComponent }       from './app.component';
 
-import { ContentPageComponent }        from '../pages/content/content-page.component';
-import { ItemCreatePage }              from '../pages/item-create/item-create';
-import { ItemDetailPage }              from '../pages/item-detail/item-detail';
-import { DepotsPageComponent }         from '../pages/depots/depots-page.component';
-import { DepotObjectsPageComponent }   from '../pages/depot-objects/depot-objects-page.component';
-import { DepotsCreatePageComponent }   from '../pages/depots-create/depots-create-page.component';
-import { LoginPageComponent }          from '../pages/login/login-page.component';
-import { SearchPageComponent }         from '../pages/search/search-page.component';
-import { SettingsPageComponent }       from '../pages/settings/settings-page.component';
-import { SignupPageComponent }         from '../pages/signup/signup-page.component';
-import { TutorialPageComponent }       from '../pages/tutorial/tutorial-page.component';
-import { WelcomePageComponent }        from '../pages/welcome/welcome-page.component';
-import { MembersPageComponent }        from '../pages/members/members-page.component';
-import { MemberCreatePageComponent }   from '../pages/members-create/members-create-page.component';
+import { ContentPageComponent }             from '../pages/content/content-page.component';
+import { ItemCreatePage }                   from '../pages/item-create/item-create';
+import { ItemDetailPage }                   from '../pages/item-detail/item-detail';
+import { MembersPageComponent }             from '../pages/members/members-page.component';
+import { MemberCreatePageComponent }        from '../pages/members-create/members-create-page.component';
+import { DepotsPageComponent }              from '../pages/depots/depots-page.component';
+import { DepotsCreatePageComponent }        from '../pages/depots-create/depots-create-page.component';
+import { DepotObjectsPageComponent }        from '../pages/depot-objects/depot-objects-page.component';
+import { DepotObjectsCreatePageComponent }  from '../pages/depot-objects-create/depot-objects-create-page.component';
+import { LoginPageComponent }               from '../pages/login/login-page.component';
+import { SearchPageComponent }              from '../pages/search/search-page.component';
+import { SettingsPageComponent }            from '../pages/settings/settings-page.component';
+import { SignupPageComponent }              from '../pages/signup/signup-page.component';
+import { TutorialPageComponent }            from '../pages/tutorial/tutorial-page.component';
+import { WelcomePageComponent }             from '../pages/welcome/welcome-page.component';
 
-import { Api }              from '../providers/api';
-import { Items }            from '../mocks/providers/items';
-import { SettingsService }  from '../providers/settings.service';
-import { UserService }      from '../providers/user.service';
-import { MemberService }    from '../providers/member.service';
-import { DepotService }     from '../providers/depot.service';
-import { DepotObjectService }     from '../providers/depot-object.service';
+import { Api }                  from '../providers/api';
+import { Items }                from '../mocks/providers/items';
+import { SettingsService }      from '../providers/settings.service';
+import { UserService }          from '../providers/user.service';
+import { MemberService }        from '../providers/member.service';
+import { DepotService }         from '../providers/depot.service';
+import { DepotObjectService }   from '../providers/depot-object.service';
 
 import { Camera }       from '@ionic-native/camera';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -51,6 +52,7 @@ export function HttpLoaderFactory(http: Http) {
     ItemDetailPage,
     DepotsPageComponent,
     DepotObjectsPageComponent,
+    DepotObjectsCreatePageComponent,
     DepotsCreatePageComponent,
     LoginPageComponent,
     SearchPageComponent,
@@ -82,6 +84,7 @@ export function HttpLoaderFactory(http: Http) {
     ItemDetailPage,
     DepotsPageComponent,
     DepotObjectsPageComponent,
+    DepotObjectsCreatePageComponent,
     DepotsCreatePageComponent,
     LoginPageComponent,
     SearchPageComponent,
