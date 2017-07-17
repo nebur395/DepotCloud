b41633d93658cc585c02035607ad2f4d6f32fd9f
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-07-17 16:18:12 +0200

    Added depotObject search

--- routes/depotObject/depotObject.js
+++ routes/depotObject/depotObject.js
@@ -41,7 +41,7 @@ module.exports = function (app) {
      *         type: string
      *     responses:
      *       200:
-     *         description: Lista con todos los almacenes de la unidad familiar.
+     *         description: Lista con todos los objetos de la unidad familiar.
      *         schema:
      *           type: object
      *           properties:
@@ -132,6 +132,121 @@ module.exports = function (app) {
                     });
                 });
             }
+        });
+    });
+
+    /**
+     * @swagger
+     * /depotObjects/search/{owner}/{name}:
+     *   get:
+     *     tags:
+     *       - DepotObject
+     *     summary: Buscar objetos.
+     *     description: Busca todos los objetos pertenecientes a {owner} que contengan la
+     *       palabra {name} (siendo una búsqueda case-insensitive).
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: owner
+     *         description: Email al que pertenecen los objetos que se desean buscar.
+     *         in: path
+     *         required: true
+     *         type: string
+     *       - name: name
+     *         description: String que está contenido en los objetos que se desean buscar
+     *           (case-insensitive).
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Lista con todos los objetos de la unidad familiar.
+     *         schema:
+     *           type: object
+     *           properties:
+     *              depotObjects:
+     *               type: array
+     *               items:
+     *                $ref: '#/definitions/DepotObject'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       404:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
+    router.get("/search/:owner/:name", function (req, res) {
+
+        // Sets the mongo database connection to gridfs in order to store and retrieve files in the DB.
+        gfs = grid(mongoose.connection.db);
+
+        DepotObject.find({owner: req.params.owner, name: {$regex: req.params.name, $options: "i"}}, function (err, depotObjectResult) {
+
+            if (err) {
+                return res.status(500).send({
+                    "success": false,
+                    "message": "Error interno del servidor."
+                });
+            }
+
+            var depotObjects = [];
+            // Iterates all the DepotObjects stored in the system
+            async.each(depotObjectResult, function (depotObject, callback) {
+
+                // User to be sent in the response
+                var depotObjectResponse = {
+                    "_id": depotObject._id,
+                    "name": depotObject.name,
+                    "owner": depotObject.owner,
+                    "depot": depotObject.depot,
+                    "guarantee": depotObject.guarantee,
+                    "dateOfExpiry": depotObject.dateOfExpiry,
+                    "description": depotObject.description
+                };
+                // Checks if there's an image attached to the DepotObject and retrieves
+                // it if it's the case.
+                if (depotObject.image) {
+                    retrieveImage(depotObject.image, function (data) {
+                        depotObjectResponse.image = data;
+                        depotObjects.push(depotObjectResponse);
+                        callback();
+                    });
+                } else {
+                    depotObjectResponse.image = "";
+                    depotObjects.push(depotObjectResponse);
+                    callback();
+                }
+
+            }, function (err) {
+
+                if (err) {
+                    return res.status(500).send({
+                        "success": false,
+                        "message": "Error interno del servidor."
+                    });
+                }
+
+                return res.status(200).send({
+                    "depotObjects": depotObjects
+                });
+            });
         });
     });
 
